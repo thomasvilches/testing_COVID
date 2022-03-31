0bdf1d724f134aa57ac8313ae1451da7f3d5fccd
thomasvilches <thomvilches@gmail.com>
2022-03-30 21:53:15 -0400

    first scenarios

--- scen.jl
+++ scen.jl
@@ -1,59 +1,45 @@
 #for the paper
-#Calibrating to R0 = 0.8-0.9 without testing
-dc = [1]
-rc = [1.0]
+
 #run_param_scen_cal(b,province,h_i,ic1,strains,index,scen,tra,eb,wpt,dayst,rc,dc,mt,vac,nsims)
-run_param_scen_cal(0.042,"ontario",10,1,3,2,0,0,0,100,[1],1.0,0.0,50)
-
-
-run_param_scen_cal(0.035,"ontario",10,1,6,1,0,0,0,100,[1],1.0,0.0,500)
+#run_param_scen_cal(0.042,"ontario",10,1,3,2,0,0,0,100,[1],1.0,0.0,50)
+#run_param_scen_cal(0.042,"ontario",10,1,3,1,0,0,0,100,[1],1.0,0.0,500)
 #0.35 for omicron generates a R0 of 0.84
 
-##### Scenarios
-#for the paper
-dc = [1]
-rc = [1.0]
 #run_param_scen_cal(b,province,h_i,ic1,strains,index,scen,tra,eb,wpt,dayst,rc,dc,mt,vac,nsims)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,0,0,0,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,1,1,0,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,2,2,0,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,2,3,0,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,2,4,0,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,3,1,0,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,4,2,0,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,4,3,0,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,4,4,0,100,[1,4],1.0,0.0,300)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,0,0,0,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,1,1,0,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,2,2,0,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,2,3,0,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,2,4,0,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,3,1,0,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,4,2,0,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,4,3,0,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,4,4,0,100,[1,4],1.0,0.0,500)
 
 
 
-#for the paper
-dc = [1]
-rc = [1.0]
 #run_param_scen_cal(b,province,h_i,ic1,strains,index,scen,tra,eb,wpt,dayst,rc,dc,mt,vac,nsims)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,0,0,20,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,1,1,20,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,2,2,20,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,2,3,20,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,2,4,20,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,3,1,20,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,4,2,20,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,4,3,20,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,4,4,20,100,[1,4],1.0,0.0,300)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,0,0,20,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,1,1,20,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,2,2,20,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,2,3,20,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,2,4,20,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,3,1,20,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,4,2,20,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,4,3,20,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,4,4,20,100,[1,4],1.0,0.0,500)
 
 
-#for the paper
-dc = [1]
-rc = [1.0]
 #run_param_scen_cal(b,province,h_i,ic1,strains,index,scen,tra,eb,wpt,dayst,rc,dc,mt,vac,nsims)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,0,0,80,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,1,1,80,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,2,2,80,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,2,3,80,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,2,4,80,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,3,1,80,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,4,2,80,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,4,3,80,100,[1,4],1.0,0.0,300)
-run_param_scen_cal(0.05,"ontario",25,1,1,1,4,4,80,100,[1,4],1.0,0.0,300)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,0,0,80,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,1,1,80,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,2,2,80,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,2,3,80,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,2,4,80,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,3,1,80,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,4,2,80,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,4,3,80,100,[1,4],1.0,0.0,500)
+run_param_scen_cal(0.042,"ontario",10,1,3,1,4,4,80,100,[1,4],1.0,0.0,500)
 
 
 
